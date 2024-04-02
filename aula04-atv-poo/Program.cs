88ab4c3116558ac7fd6fa8fce2ccfa1ff14b4fc4
Pia <algauer.henrique@gmail.com>
2024-04-01 21:16:28 -0300

    requisitos exercicio

---
+++ aula04-atv-poo/Program.cs
@@ -0,0 +1,17 @@
+﻿/* 
+Atividade ORIENTAÇÃO OBJETO 
+1.Classe produto
+Atributos (Codigo, nome, Preco)
+2.Classe Cliente
+Atributos (CPF, NOME)
+3.Classe vendedor
+At (Matricula, cpf, nome)
+4.Classe Venda
+At (Vendedor, Cliente, lista_de_produtos_vendidos, total_venda)
+
+Métodos: 
+a. Construtor com dependência do Vendedor e do Cliente
+b. Construtor padrão (que constrói o objeto sem estado)
+c. Método para adicionar os produtos comprados pelo cliente
+d. Método para calcular o valor total da venda
+*/
