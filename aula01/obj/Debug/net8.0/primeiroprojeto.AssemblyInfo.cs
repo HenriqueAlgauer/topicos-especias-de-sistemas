c52cb257cc628c48b693a7d1763fb86beacf2bd5
henriqueAlgauer <algauer.henrique@gmail.com>
2024-05-06 10:21:33 -0300

    alteração

--- aula01/obj/Debug/net8.0/primeiroprojeto.AssemblyInfo.cs
+++ aula01/obj/Debug/net8.0/primeiroprojeto.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< HEAD
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa2772832cf8d554530555258df87253040c4bf1")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa990af06c2c72a30bf2558a99395145e0923f1e")]
+>>>>>>> 5484b56968e1b65d6cc7ab02310a5fc1f48518dc
 [assembly: System.Reflection.AssemblyProductAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("primeiroprojeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
